d1cc8bd45fd86694c92157be93a48f629a4d0a37
nlxxtw <1071632864@qq.com>
2024-04-09 13:38:39 +0800

    Site updated: 2024-04-09 13:38:38

--- js/welcome.js
+++ js/welcome.js
@@ -3,7 +3,7 @@ function welcome() {
   let lastVisitTime = localStorage.getItem('lastVisitTime');
   let currentTime = new Date().getTime();
   
-  if (!lastVisitTime || (currentTime - lastVisitTime > 120000)) {
+  if (!lastVisitTime || (currentTime - lastVisitTime > 27000)) { // 第一次访问或距上次访问超过30秒
     if (document.referrer !== '') {
       let referrer = document.referrer.split("/")[2];
       welcome_text = "欢迎你，来自" + referrer.toUpperCase() + "的朋友！";
@@ -17,18 +17,20 @@ function welcome() {
       
       let locationInfo = country + ' ' + city;
       
-      swal({
-        title: " 欢迎关注公众号学习帮pro！",
-        text: welcome_text + '\n欢迎来自' + locationInfo + '的用户！\n获取更多资源请扫码关注！',
-        imageUrl: "https://cdn.jsdelivr.net/gh/nlxxtw/cdn@main/gzh.png",
-        showConfirmButton: true
-      }).then((result) => {
-        if (result.isConfirmed) {
-          // 用户点击了确认按钮后执行的操作
-          // 可以在这里添加需要执行的代码
-          swal.close(); // 关闭弹窗
-        }
-      });
+      setTimeout(() => {
+        swal({
+          title: " 欢迎关注公众号学习帮pro！",
+          text: welcome_text + '\n欢迎来自' + locationInfo + '的用户！\n获取更多资源请扫码关注！',
+          imageUrl: "https://cdn.jsdelivr.net/gh/nlxxtw/cdn@main/gzh.png",
+          showConfirmButton: true
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // 用户点击了确认按钮后执行的操作
+            // 可以在这里添加需要执行的代码
+            swal.close(); // 关闭弹窗
+          }
+        });
+      }, 27000); // 等待30秒后显示弹窗
     });
     
     localStorage.setItem('lastVisitTime', currentTime);
@@ -37,4 +39,17 @@ function welcome() {
 
 $(document).ready(() => {
   welcome();
+  
+  let refreshTime = 0;
+  $(window).on("beforeunload", function() {
+    refreshTime = new Date().getTime();
+  });
+
+  $(window).on("load", function() {
+    setTimeout(() => {
+      if (refreshTime !== 0 && new Date().getTime() - refreshTime > 120000) {
+        welcome();
+      }
+    }, 0);
+  });
 });
